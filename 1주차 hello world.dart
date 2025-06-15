7a7720f7d99772e21df3f74c525078ed92077a5a
aajwp <aajwp1@g.shingu.ac.kr>
2025-06-16 02:22:45 +0900

    Update 1주차 hello world.dart

--- 1주차 hello world.dart
+++ 1주차 hello world.dart
@@ -1,3 +1,13 @@
+<h2>1주차</h2>
+
+- Android Studio에 flutter 플러그인 설치
+- flutter sdk 설치
+- 환경변수 설정
+- hello_world 프로젝트 생성 후 코드작성
+- 디바이스 크롬 연결 후 결과확인
+
+---
+
 import 'package:flutter/material.dart';
 
 void main() {
